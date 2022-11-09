476ad454b8c4dece6f7933007db6b86aa99f644f
MPilecarte <mayara.pilecarte@gmail.com>
2022-11-08 21:54:55 -0300

    New project 'Affirmatiion'

---
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.affirmations.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.textview)
+        textView.text = Datasource().loadAffirmations().size.toString() /*criado uma Datasource chamando o metodo loadAffirmation(), acessando o tamanho da lista e transformando em uma string ao atribuir à textView*/
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,8 @@
+package com.example.affirmations.adapter
+
+import com.example.affirmations.model.Affirmation
+
+class ItemAdapter(private val dataset: List<Affirmation>) {
+
+
+}
